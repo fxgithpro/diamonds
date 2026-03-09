2950c9a4be12c19f9201990cb3ad1905b5886be9
edouard steiner <boursoracle@free.fr>
2026-03-09 17:22:04 +0100

    Implementation of functions to instantite, train and evaluate a model

--- src/diamonds/model.py
+++ src/diamonds/model.py
@@ -1,4 +1,15 @@
-from sklearn.base import BaseEstimator, Pipeline
+# ML
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import LinearRegression, ElasticNet
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from soupsieve import match
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 
 
 def create_model(model_name: str) -> BaseEstimator:
@@ -14,22 +25,54 @@ def create_model(model_name: str) -> BaseEstimator:
     -------
     BaseEstimator
         The model ready to be fitted
-    """
-    pass
+    """ 
+
+    if model_name == "LinearRegression":
+         return LinearRegression()
+    elif model_name == "random_forest":
+        return RandomForestRegressor(n_estimators=100, max_depth=10)
+    elif model_name == "KNN":
+        return KNeighborsRegressor(n_neighbors=5)
+    elif model_name == "SVR":
+        return SVR(kernel='rbf', C=1.0, epsilon=0.1)
+    else : return None
 
 def create_preproc() -> Pipeline:
     """
     Create a preprocessing pipeline.
     """
-    pass
+    cat_pipe = Pipeline(
+    [ ("cat_imp",SimpleImputer(strategy="most_frequent"))
+      ,("ohe",OneHotEncoder(drop="first",sparse_output=False))
+        ])
+
+    num_pipe = Pipeline(
+    [("knn_imp", KNNImputer(n_neighbors=5))
+     ,("scaler", StandardScaler())
+      ])
+
+    preprocessor = ColumnTransformer(
+    [("numeric",num_pipe, make_column_selector(dtype_include="number"))
+    ,("categorical", cat_pipe, make_column_selector(dtype_exclude="number"))
+      ]).set_output(transform="pandas")
+
+    return preprocessor
 
 def train_model(model, X_train, y_train):
-    pass
+    trained_model = model.fit(X_train, y_train)
+    return trained_model
 
 def evaluate_model(model, X_test, y_test) -> dict[str, float]:
     # NB : mae, mse, r2_score, mape
     # Only print the metrics for now
-    pass
+    y_pred = model.predict(X_test)
+    mae = mean_absolute_error(y_test,y_pred)
+    mse = mean_squared_error(y_test,y_pred)
+    r2  = r2_score(y_test,y_pred) 
+    mape = mean_absolute_percentage_error(y_test,y_pred)
+    scores = {"mae":mae,"mse":mse,"r2":r2,"mape":mape}
+    print(scores)
+    return scores
 
 def predict(model, X):
     """
@@ -47,4 +90,18 @@ def predict(model, X):
     pd.Series
         The predicted values
     """
-    
+    y_pred = model.predict(X)
+    return y_pred
+
+if __name__ == "__main__":
+    import seaborn as sns
+    print("Creating model...")
+    model = create_model("random_forest")
+    print(model)
+    print("Creating preprocessor...")
+    toto =sns.load_dataset("diamonds")
+    titi=toto.drop(columns=["price"])
+    preprocessor= create_preproc()
+    tata=preprocessor.fit_transform(titi)
+    print(tata)
+    print(preprocessor)
